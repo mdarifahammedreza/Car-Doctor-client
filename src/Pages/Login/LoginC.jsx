fc9d19a127e7352c1f0d0268a3bcdc09ad0da82e
Md Arif Ahammed Reza <mdarifahammedreza@gmail.com>
2024-05-02 15:03:56 +0600

    Login page created

---
+++ src/Pages/Login/LoginC.jsx
@@ -0,0 +1,13 @@
+import NavbarWithoutLogin from "../../Components/Share/NavbarWithoutLogin";
+import Login from "./Login";
+
+const LoginC = () => {
+  return (
+    <div className="md:mx-20">
+      <NavbarWithoutLogin />
+      <Login />
+    </div>
+  );
+};
+
+export default LoginC;
--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../Pages/Root";
 import Loader from "../Components/Loader/Loader";
 import Home from "../Pages/Home/Home";
+import LoginC from "../Pages/Login/LoginC";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,16 @@ const router = createBrowserRouter([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "/login",
+    element: <LoginC />,
+    // children: [
+    //   {
+    //     path: "/",
+    //     element: <Home />,
+    //   },
+    // ],
   },
 ]);
 
