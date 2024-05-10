b5da9946d954148a39bf20c298bd734006e531b0
Md Arif Ahammed Reza <mdarifahammedreza@gmail.com>
2024-05-10 14:13:48 +0600

    worked on log in

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,20 +1,43 @@
+import { useState } from "react";
 import loginImage from "../../../src/assets/images/login/login.svg";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 const Login = () => {
+
+
+  // Login Handeler
+  //
+  const LoginHandeler = (e) => {
+    e.preventDefault();
+    const Email = e.target.email.value;
+    const Password = e.target.password.value;
+    const User = { Email, Password };
+    if(Password.length < 8){
+      toast.error("Password must be at least 7 characters long");
+    }
+  };
+  //
+  //
+  // Password Show Hide
+  const [show_Password, Set_Password_Status] = useState(true);
+  //
+  //
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row">
-        <div className="sm:hidden md:contents">
-          <img src={loginImage} alt="" />
+    <div className="hero min-h-screen bg-base-100">
+      <div className="flex  justify-around items-center gap-10">
+        <div className="hidden sm:block lg:contents">
+          <img src={loginImage} alt="img" />
         </div>
-        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
-            <h1 className="text-5xl font-bold">Login now!</h1>
+        <div className=" shadow-2xl bg-transparent ">
+          <form className="card-body" onSubmit={LoginHandeler}>
+            <h1 className="text-4xl font-bold">Login now!</h1>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
-                type="email"
+              id="email"
                 placeholder="email"
                 className="input input-bordered"
                 required
@@ -24,20 +47,42 @@ const Login = () => {
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
+              <div>
+                <div className="relative flex">
+                  <input
+                    type={show_Password ? "text" : "password"}
+                    placeholder="password"
+                    id="password"
+                    className="input input-bordered"
+                    required
+                  />
+                  <div className="absolute right-3 bottom-4">
+                    {show_Password ? (
+                      <FaEyeSlash
+                        onClick={() => Set_Password_Status(!show_Password)}
+                        className="cursor-pointer"
+                      />
+                    ) : (
+                      <FaEye
+                        onClick={() => Set_Password_Status(!show_Password)}
+                        className="cursor-pointer"
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button
+                className="btn  bg-orange-600 text-white hover:bg-orange-500"
+                type="submit">
+                Login
+              </button>
+              <Toaster />
+              <Link className="text-xs font-semibold mt-1">
+                Forget password?
+                <span className="underline  text-blue-600">Click here</span>
+              </Link>
             </div>
           </form>
         </div>
